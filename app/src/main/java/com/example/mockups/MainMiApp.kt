050dba0f56322e4075f82bff7c438253984e2d5a
Yordy Nuñez <yordierik05@gmail.com>
2024-05-24 17:34:00 -0500

    Se agrego el login

--- app/src/main/java/com/example/mockups/MainMiApp.kt
+++ app/src/main/java/com/example/mockups/MainMiApp.kt
@@ -1,29 +1,26 @@
 package com.example.mockups
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class MainMiApp : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main_mi_app)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
+        var btn_volver =findViewById<Button>(R.id.btn_login)
+        btn_volver.setOnClickListener {
+            //finish elimina o cierra la actividad actual
+            finish()
+        }
     }
-    fun cambioLogin(view: View) {
-        var intent = Intent(application, login::class.java)
-        startActivity(intent)
-    }
-
-
 }
--- app/src/main/java/com/example/mockups/login.kt
+++ app/src/main/java/com/example/mockups/login.kt
@@ -1,6 +1,9 @@
 package com.example.mockups
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +19,15 @@ class login : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        var btn_salir =findViewById<Button>(R.id.btn_salir)
+        btn_salir.setOnClickListener {
+            //finish elimina o cierra la actividad actual
+            finish()
+        }
+
+    }
+    fun btn_app(view: View) {
+        var intent = Intent(application, MainMiApp::class.java)
+        startActivity(intent)
     }
 }
